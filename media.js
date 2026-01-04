49a97e7a7ba9f04e7c0c2e4e6fe296e75f4b3ea3
Zubair <zubairibnarif@gmail.com>
2026-01-04 12:52:00 +0500

    Update media.js

--- media.js
+++ media.js
@@ -1,18 +1,16 @@
-
 const archiveMedia = [
-    
     {
         type: 'video',
         src: 'contents/africa.mp4',
         caption: 'Death over submission',
-        aspect: 'portrait' ,
+        aspect: 'portrait',
         date: '2025-12-22'
     },
     {
         type: 'image',
         src: 'contents/goat.jpg',
         caption: 'Three goats in Bannu, Khyber Pakhtunkhwa',
-        aspect: 'landscape' ,
+        aspect: 'landscape',
         date: '2025-12-22'
     },
     {
@@ -28,16 +26,14 @@ const archiveMedia = [
         caption: '"My religion teaches that"',
         aspect: 'portrait',
         date: '2025-12-22'
-    },    {
+    },
+    {
         type: 'video',
         src: 'https://www.youtube.com/embed/qNfAFfu6VD0',
         caption: 'Democracy is Hypocrisy',
-        aspect: 'landscape' ,
+        aspect: 'landscape',
         date: '2025-12-23'
-    },  
-
-    
-
+    },
     {
         type: 'image',
         src: 'contents/pointing.jpg',
@@ -45,54 +41,41 @@ const archiveMedia = [
         aspect: 'potrait',
         date: '2025-12-24'
     },
-
-      {
+    {
         type: 'video',
         src: 'contents/favscene.mp4',
-        caption: 'My all time favourite movie scene in one of my favourite movie ever. A true hidden gem: The Assassination of Jesse James by the Coward Robert Ford (2007) by Andrew Dominik',
-        aspect: 'landscape' ,
+        caption: 'My all time favourite movie scene...',
+        aspect: 'landscape',
         date: '2025-12-26'
     },
-
-     {
+    {
         type: 'video',
         src: 'contents/ob1.mp4',
-       caption: 'For we don`t know what will happen later',
-        aspect: 'landscape' ,
+        caption: 'For we don`t know what will happen later',
+        aspect: 'landscape',
         date: '2026-01-01'
     },
-
-
-  
-
-     {
+    {
         type: 'video',
         src: 'contents/ob2.mp4',
         caption: 'Zargiya Lewaneeya',
-        aspect: 'landscape' ,
+        aspect: 'landscape',
         date: '2026-01-01'
     },
-
     {
         type: 'video',
         src: 'contents/fallujah.mp4',
-       caption: 'Death to American Imperialism',
-        aspect: 'landscape' ,
+        caption: 'Death to American Imperialism',
+        aspect: 'landscape',
         date: '2026-01-04'
-    },
-
-
-
-  
+    }
 ];
-
 
 function displayMedia() {
     const grid = document.getElementById('media-grid-dynamic');
     if (!grid) return; 
-    
+
     grid.innerHTML = archiveMedia.map(item => {
-        // This line converts '2025-12-22' into '22 Dec 2025'
         const formattedDate = item.date 
             ? new Date(item.date).toLocaleDateString('en-GB', { 
                 day: 'numeric', 
@@ -113,12 +96,16 @@ function displayMedia() {
                 </div>
                 <div class="media-info">
                     <p class="media-desc">${item.caption}</p>
-                    <time class="media-date">${formattedDate}</time>
+                    <time class="media-date" style="display:block; font-size:0.8rem; color:gray; margin-top:5px;">
+                        ${formattedDate}
+                    </time>
                 </div>
             </div>
         `;
     }).join('');
 }
 
+
+document.addEventListener('DOMContentLoaded', displayMedia);
 
 
